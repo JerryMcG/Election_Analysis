021b401330b391d4c85739fb505b4278e45ea56f
JerryMcG <76828978+JerryMcG@users.noreply.github.com>
2021-07-10 18:41:45 -0400

    Analysis Complete - Winner identified

--- PyPoll.py
+++ PyPoll.py
@@ -19,6 +19,12 @@ total_votes = 0
 candidate_options = []
 # initialize candidate dictionary
 candidate_votes = {}
+#empty string for winning candidate
+winning_candidate = ""
+#create winning count variable
+winning_count = 0
+#create winning percentage variable
+winning_percentage = 0
 
 #open the election results and read the file
 with open(file_to_load) as election_data:
@@ -41,9 +47,36 @@ with open(file_to_load) as election_data:
         #add candidate votes
         candidate_votes[candidate_name] +=1
 
+#get votes for each candidate and calc percentage
+for candidate_name in candidate_votes:
+    #get votes for each candidate
+    votes = candidate_votes[candidate_name]
+    #calc % of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #print candidate name and % votes
+    print(f"{candidate_name} recieved {vote_percentage:.1f}% of the votes ({votes:,})\n")
+    #determine winning vote count and candidate
+    #is vote count greater than winning_vote
+    if votes > winning_count and vote_percentage > winning_percentage:
+        #if true - set values to winning counts/percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #set winning candidate name equal to candidate name
+        winning_candidate = candidate_name
+
+#print winners details
+winning_candidate_summary = (
+    f"------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Vote Percentage: {winning_percentage:.1f}%\n"
+    f"------------------------------\n"
+)
+print(winning_candidate_summary)
+     
 #3. Print the total votes
-print(total_votes)
+#print(total_votes)
 #print Candidate names
-print(candidate_options)
+#print(candidate_options)
 #print candidate votes
-print(candidate_votes)
+#print(candidate_votes)
